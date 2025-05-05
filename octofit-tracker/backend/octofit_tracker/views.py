8ae5a3861be97c4ffd8b5bb8fed00d89045897f0
Chenguang Xu <xcg1234@gmail.com>
2025-05-05 16:25:21 +0000

    Added new features and db

---
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
+from .models import User, Team, Activity, Leaderboard, Workout
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': 'http://localhost:8000/api/users/',
+        'teams': 'http://localhost:8000/api/teams/',
+        'activities': 'http://localhost:8000/api/activities/',
+        'leaderboard': 'http://localhost:8000/api/leaderboard/',
+        'workouts': 'http://localhost:8000/api/workouts/',
+    })
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+class LeaderboardViewSet(viewsets.ModelViewSet):
+    queryset = Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
